488ba697df5befd2a83e43a018dc44648db75736
J-1ac <tkdwns26@naver.com>
2022-08-02 23:38:14 +0900

    commit

---
+++ EX_Cpp/NameSp2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+namespace BestComImpl{
+    void SimpleFunc(void);
+}
+
+namespace ProComImpl{
+    void SimpleFunc(void);
+}
+
+int main(void){
+    BestComImpl::SimpleFunc();
+    ProComImpl::SimpleFunc();
+    return 0;
+}
+
+void BestComImpl::SimpleFunc(void){
+    std::cout<<"BestCom이 정의한 함수"<<std::endl;
+}
+
+void ProComImpl::SimpleFunc(void){
+    std::cout<<"ProCom이 정의한 함수"<<std::endl;
+}
